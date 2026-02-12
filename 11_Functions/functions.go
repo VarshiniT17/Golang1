c161bfaa04009701fd7abe473efd8e35c3d79eb0
VarshiniT17 <varshiniturmari17@gmail.com>
2026-02-12 21:35:14 +0530

    updated files

--- 11_Functions/functions.go
+++ 11_Functions/functions.go
@@ -2,20 +2,44 @@ package main
 
 import "fmt"
 
-// func simplefunction() {
-// 	fmt.Println("simplefunction")
+func simplefunction() {
+	fmt.Println("simplefunction")
+}
+
+// func add(a, b int) (result int) {
+// 	result = a + b
+// 	return
+
 // }
 
-func add(a, b int) (result int) {
-	result = a + b
-	return
+// func getlang() (string, string, bool) {
+// 	return "Go", "Python", true
+// }
 
-}
+// func process(fn func(a int)int){
+// 	fn(1)
+// }
+
 func main() {
 
 	// fmt.Println("learning functions  ")
 	// simplefunction()
 
-	result := add(10, 3)
-	fmt.Println("sum =", result)
+	// result := add(10, 3)
+	// fmt.Println("sum =", result)
+
+	// lang1, lang2, lang3 := getlang()
+	// fmt.Println(lang1, lang2, lang3)
+
+	// x, y, _ := getlang()
+	// fmt.Println(x, y)
+
+	// fn :=func (a int)int{
+	// 	return 2
+	// }
+
+	// fmt.Println
 }
+
+///functions are also first class function can be assigned to a variables also
+//or can also be assigned to other function also
