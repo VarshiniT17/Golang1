e058ab8a2fc6732cab3a266564c339e5e9f07ec0
VarshiniT17 <varshiniturmari17@gmail.com>
2026-02-16 12:41:34 +0530

    updated files

--- 16_pointers/pointers.go
+++ 16_pointers/pointers.go
@@ -1,35 +1,53 @@
+// package main
+
+// import "fmt"
+
+// ///copied by value
+// // func changenum(num int) {
+// // 	num = 5
+// // 	fmt.Println("in changenum", num)
+// // }
+
+// //by refrence
+// func changenum(num *int) {
+// 	//de refer
+// 	*num = 5
+// 	fmt.Println("in changenum", *num)
+// }
+
+// func main() {
+// 	///this variable has some value which will never change
+// 	///the value doesnot change for the variables present in the main function even if you change it in other functions
+
+// 	// even if the value changes for the same variable in other function
+// 	//
+// 	num := 1
+
+// 	// changenum(num)
+// 	changenum(&num)
+// 	fmt.Println("after in chnangenum ", num)
+
+// 	// fmt.Println("memory address", &num)
+// 	// fmt.Println("after in chnangenum ", num)
+// 	//to change the value of the variables present in the main function using the same variables present in the other functions we pass the reference of the variable
+// 	//in the main function to the variable in the different function (address of the variable of the main function)
+
+// }
+
 package main
 
 import "fmt"
 
-///copied by value
-// func changenum(num int) {
-// 	num = 5
-// 	fmt.Println("in changenum", num)
-// }
-
-//by refrence
-func changenum(num *int) {
-	//de refer
-	*num = 5
-	fmt.Println("in changenum", *num)
-}
-
 func main() {
-	///this variable has some value which will never change
-	///the value doesnot change for the variables present in the main function even if you change it in other functions
+	fmt.Println("pointers learning")
 
-	// even if the value changes for the same variable in other function
 	//
-	num := 1
+	x := 10
+	ptr := &x
+	ptr1 := &ptr
 
-	// changenum(num)
-	changenum(&num)
-	fmt.Println("after in chnangenum ", num)
-
-	// fmt.Println("memory address", &num)
-	// fmt.Println("after in chnangenum ", num)
-	//to change the value of the variables present in the main function using the same variables present in the other functions we pass the reference of the variable
-	//in the main function to the variable in the different function (address of the variable of the main function)
+	fmt.Println(ptr1)
+	fmt.Println(ptr)
+	fmt.Println(x)
 
 }
