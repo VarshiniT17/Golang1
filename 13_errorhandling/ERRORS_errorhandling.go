701bdca8e4fd82fa0635f0fcedc8ede325603a88
VarshiniT17 <varshiniturmari17@gmail.com>
2026-02-25 09:10:07 +0530

    New files modified

--- 13_errorhandling/ERRORS_errorhandling.go
+++ 13_errorhandling/ERRORS_errorhandling.go
@@ -1,68 +1,66 @@
-package main
+// // package main
 
-import "fmt"
+// // import "fmt"
 
-// func divide(a, b float64) (float64, error) {
-// 	if b == 0 {
-// 		return 0, fmt.Errorf("denominator cannot be zero")
-// 	}
-// 	return a / b, nil
+// // func divide(a, b float64) (float64, error) {
+// // 	if b == 0 {
+// // 		return 0, fmt.Errorf("denominator cannot be zero")
+// // 	}
+// // 	return a / b, nil
 
+// // }
+// // func main() {
+// // 	// fmt.Println("Error handling")
+
+// // 	data, _ := divide(10, 2)
+// // 	fmt.Println("answer= ", data)
+// // }
+// ////////////////////////////////////////////////////////////////////////
+// // package main
+
+// // import "fmt"
+
+// // func sum(a, b int) (result int, err error) {
+
+// // 	if a < 0 || b < 0 {
+// // 		return 0, errors.New("values cannot be zero or less than zero")
+// // 	}
+// // 	return a + b, nil
+// // }
+// // func main() {
+// // 	c, err := sum(10, -1)
+
+// // 	if err != nil {
+// // 		fmt.Println(err.Error())
+// // 	}
+// // }
+
+// //In Go, errors are values.
+
+// //Go does NOT use try-catch like Java/Python.
+// //Instead, functions return an error as the last return value.
+// // value, err := someFunction()
+// // if err != nil {
+// //     // handle error
+// // }
+
+// // The error Interface
+// // In Go, error is just an interface:
+
+// type error interface {
+//     Error() string
 // }
-// func main() {
-// 	// fmt.Println("Error handling")
+// Any type that implements Error() string is an error.
 
-// 	data, _ := divide(10, 2)
-// 	fmt.Println("answer= ", data)
-// }
-////////////////////////////////////////////////////////////////////////
-// package main
+// Creating Errors
+// ✅ Using errors.New()
+// import "errors"
 
+// err := errors.New("something went wrong")
+// ✅ Using fmt.Errorf()
 // import "fmt"
 
-// func sum(a, b int) (result int, err error) {
-
-// 	if a < 0 || b < 0 {
-// 		return 0, errors.New("values cannot be zero or less than zero")
-// 	}
-// 	return a + b, nil
-// }
-// func main() {
-// 	c, err := sum(10, -1)
-
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// }
-
-//In Go, errors are values.
-
-//Go does NOT use try-catch like Java/Python.
-//Instead, functions return an error as the last return value.
-// value, err := someFunction()
-// if err != nil {
-//     // handle error
-// }
-
-The error Interface
-In Go, error is just an interface:
-
-type error interface {
-    Error() string
-}
-Any type that implements Error() string is an error.
-
-
-
-Creating Errors
-✅ Using errors.New()
-import "errors"
-
-err := errors.New("something went wrong")
-✅ Using fmt.Errorf()
-import "fmt"
-
-err := fmt.Errorf("user %d not found", id)
+// err := fmt.Errorf("user %d not found", id)
 
 
 
